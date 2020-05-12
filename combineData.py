8725e72dbe45642a978acc3468c091548d92bdca
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2020-05-12 01:51:14 -0700

    Delete combineData.py

--- combineData.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataframe_14 = pd.read_csv('../TrainingData/processed_data14.csv', index_col=0)
-dataframe_21 = pd.read_csv('../TrainingData/processed_data21.csv', index_col=0)
-
-dataframe = pd.concat([dataframe_14, dataframe_21])
-dataframe.to_csv('../TrainingData/processed_data.csv')
-
